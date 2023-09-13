a5b9a1b3fcf41c9bd11532cab18ac02f613eb515
hojoon <ujuj0202@gmail.com>
2023-09-13 23:51:11 +0900

    [FEAT] User 엔티티와 그외 관련 Enum 구현

---
+++ src/main/java/com/project/socket/user/model/Role.java
@@ -0,0 +1,5 @@
+package com.project.socket.user.model;
+
+public enum Role {
+  ROLE_USER, ROLE_ADMIN
+}
---
+++ src/main/java/com/project/socket/user/model/User.java
@@ -0,0 +1,61 @@
+package com.project.socket.user.model;
+
+import com.project.socket.common.model.BaseTime;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@ToString
+@Getter
+@NoArgsConstructor
+public class User extends BaseTime {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long userId;
+
+  @Column
+  private String nickname;
+
+  @Column
+  private String githubLink;
+
+  @Column
+  private String email;
+
+  @Column(nullable = false)
+  private boolean profileSetup;
+
+  @Column(nullable = false)
+  private String socialId;
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false)
+  private SocialProvider socialProvider;
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false)
+  private Role role;
+
+  @Builder
+  public User(Long userId, String nickname, String githubLink, String email, boolean profileSetup,
+      String socialId, SocialProvider socialProvider, Role role) {
+    this.userId = userId;
+    this.nickname = nickname;
+    this.githubLink = githubLink;
+    this.email = email;
+    this.profileSetup = profileSetup;
+    this.socialId = socialId;
+    this.socialProvider = socialProvider;
+    this.role = role;
+  }
+}
