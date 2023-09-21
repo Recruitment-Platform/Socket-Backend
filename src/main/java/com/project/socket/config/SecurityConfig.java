0e6dffc718a2e5b1e18e131b59c1fbf7babdbe64
hojoon <ujuj0202@gmail.com>
2023-09-21 22:38:24 +0900

    [FEAT] securityConfig 설정 클래스

--- src/main/java/com/project/socket/config/SecurityConfig.java
+++ src/main/java/com/project/socket/config/SecurityConfig.java
@@ -1,26 +1,60 @@
 package com.project.socket.config;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import com.project.socket.security.oauth2.handler.OAuth2LoginFailureHandler;
+import com.project.socket.security.oauth2.handler.OAuth2LoginSuccessHandler;
+import java.util.Arrays;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @EnableWebSecurity
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-  @Bean // HttpSecurity
+  private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
+  private final OAuth2LoginFailureHandler oAuth2LoginFailureHandler;
+
+  @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
     http
+        .sessionManagement(sessionManager -> sessionManager
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .oauth2Login(oauth2Login -> oauth2Login
+            .successHandler(oAuth2LoginSuccessHandler)
+            .failureHandler(oAuth2LoginFailureHandler))
         .csrf(csrf -> csrf.disable())
+        .cors(withDefaults())
         .httpBasic(httpBasic -> httpBasic.disable())
         .formLogin(formLogin -> formLogin.disable())
-        .sessionManagement(sessionManagement ->
-            sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(authorize -> authorize
-            .anyRequest().permitAll());
+            .requestMatchers("/docs/**", "/actuator/**", "/", "/error/**").permitAll()
+            .anyRequest().authenticated());
     return http.build();
+  }
+
+  @Value("${cors.allow-origins}")
+  private List<String> corsOrigins;
+
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration corsConfiguration = new CorsConfiguration();
+    corsConfiguration.setAllowedOrigins(corsOrigins);
+    corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE", "PUT", "PATCH"));
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", corsConfiguration);
+    return source;
   }
 }
