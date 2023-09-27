3bb61c0d68748d329f12568fc00a7bb9148a3f35
hojoon <ujuj0202@gmail.com>
2023-09-28 00:07:26 +0900

    [TEST] 테스트코드 변경

--- src/test/java/com/project/socket/common/error/ProblemDetailFactoryTest.java
+++ src/test/java/com/project/socket/common/error/ProblemDetailFactoryTest.java
@@ -14,21 +14,21 @@ import org.springframework.http.ProblemDetail;
 class ProblemDetailFactoryTest {
 
   @Test
-  void 에러코드에_해당하는_ProblemDetail을_생성한다(){
+  void 에러코드에_해당하는_ProblemDetail을_생성한다() {
     ProblemDetail problemDetail = ProblemDetailFactory.from(OAUTH2_LOGIN_FAIL);
 
     assertAll(
         () -> assertThat(problemDetail.getStatus()).isEqualTo(OAUTH2_LOGIN_FAIL.getStatus()),
-        () -> assertThat(problemDetail.getDetail()).isEqualTo(OAUTH2_LOGIN_FAIL.getMessage()),
         () -> assertThat(problemDetail.getProperties().get("code"))
             .isEqualTo(OAUTH2_LOGIN_FAIL.getCode())
     );
   }
 
   @Test
-  void 에러코드와_instanceURI에_해당하는_ProblemDetail을_생성한다(){
+  void 에러코드와_instanceURI에_해당하는_ProblemDetail을_생성한다() {
     URI instanceURI = URI.create("/some/request");
-    ProblemDetail problemDetail = ProblemDetailFactory.of(OAUTH2_LOGIN_FAIL, instanceURI.toString());
+    ProblemDetail problemDetail = ProblemDetailFactory.of(OAUTH2_LOGIN_FAIL,
+        instanceURI.toString());
 
     assertAll(
         () -> assertThat(problemDetail.getStatus()).isEqualTo(OAUTH2_LOGIN_FAIL.getStatus()),
