cbfb146a0e14c5088d8bf87d2c375691f9c57ed3
hojoon <ujuj0202@gmail.com>
2023-10-03 17:17:55 +0900

    [TEST] OAuth2LoginTest 테스트방식 변경

--- src/test/java/com/project/socket/security/OAuth2LoginTest.java
+++ src/test/java/com/project/socket/security/OAuth2LoginTest.java
@@ -10,20 +10,18 @@ import static org.springframework.restdocs.request.RequestDocumentation.pathPara
 import static org.springframework.restdocs.request.RequestDocumentation.queryParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.project.socket.security.filter.JwtFilter;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(excludeFilters = @ComponentScan.Filter(
-    type = FilterType.ASSIGNABLE_TYPE, classes = JwtFilter.class))
+@SpringBootTest
+@AutoConfigureMockMvc
 @ActiveProfiles("test")
 @AutoConfigureRestDocs
 @DisplayNameGeneration(ReplaceUnderscores.class)
