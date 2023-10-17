ac29cb965f707c9cef4bacb094d7c2fa2d478366
hojoon <ujuj0202@gmail.com>
2023-10-17 13:22:32 +0900

    [FEAT] 대댓글 추가 기능 구현

--- src/main/java/com/project/socket/comment/repository/CommentJpaRepository.java
+++ src/main/java/com/project/socket/comment/repository/CommentJpaRepository.java
@@ -3,6 +3,6 @@ package com.project.socket.comment.repository;
 import com.project.socket.comment.model.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CommentJpaRepository extends JpaRepository<Comment, Long>, CommentRepositoryCustom {
+public interface CommentJpaRepository extends JpaRepository<Comment, Long> {
 
 }
