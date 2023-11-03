bff214f3f5ae5f2633f8cce268c9c380b8f7604e
ShinMinOh <luciano97@naver.com>
2023-11-04 01:45:09 +0900

    [REFACTOR] SocketApplication 필요없는 주석 제거

--- src/main/java/com/project/socket/SocketApplication.java
+++ src/main/java/com/project/socket/SocketApplication.java
@@ -3,7 +3,7 @@ package com.project.socket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@EnableJpaAuditing
+
 @SpringBootApplication
 public class SocketApplication {
 
