3543557f5c0465f1747689737265aabb25796035
ShinMinOh <luciano97@naver.com>
2023-11-04 01:43:05 +0900

    [REFACTOR] import 필요한 기능만 추가

--- src/main/java/com/project/socket/post/model/Post.java
+++ src/main/java/com/project/socket/post/model/Post.java
@@ -2,8 +2,21 @@ package com.project.socket.post.model;
 
 import com.project.socket.common.model.BaseTime;
 import com.project.socket.user.model.User;
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Getter
@@ -11,59 +24,59 @@ import lombok.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Post extends BaseTime {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "post_id")
-    private Long id;                                  //게시물 ID
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "post_id")
+  private Long id;                                  //게시물 ID
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")    // 단방향 매핑
-    private User user;
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id")    // 단방향 매핑
+  private User user;
 
-    private String title;                            // 게시글 제목
+  private String title;                            // 게시글 제목
 
-    @Column(name = "content")
-    private String postContent;                      // 게시글 내용
-
-
-    @Enumerated(EnumType.STRING)
-    @Column(name = "post_status")
-    private PostStatus postStatus;                     // 게시글 상태(CREATED/MODIFIED/DELETED)
+  @Column(name = "content")
+  private String postContent;                      // 게시글 내용
 
 
-    @Enumerated(EnumType.STRING)
-    @Column(name = "post_type")
-    private PostType postType;                     // 게시글 타입(PROJECT/STUDY)
-
-    @Enumerated(EnumType.STRING)
-    @Column(name = "post_meeting")
-    private PostMeeting postMeeting;                     // 온,오프라인 여부(ONLINE/OFFLINE/ON_OFFLINE)
+  @Enumerated(EnumType.STRING)
+  @Column(name = "post_status")
+  private PostStatus postStatus;                     // 게시글 상태(CREATED/MODIFIED/DELETED)
 
 
-    public static Post createNewPost(User user, String title, String content, PostType postType,
-                                     PostMeeting postMeeting) {
-        return Post.builder()
-                .user(user)
-                .title(title)
-                .postContent(content)
-                .postType(postType)
-                .postMeeting(postMeeting)
-                .postStatus(PostStatus.CREATED)
-                .build();
-    }
+  @Enumerated(EnumType.STRING)
+  @Column(name = "post_type")
+  private PostType postType;                     // 게시글 타입(PROJECT/STUDY)
+
+  @Enumerated(EnumType.STRING)
+  @Column(name = "post_meeting")
+  private PostMeeting postMeeting;                     // 온,오프라인 여부(ONLINE/OFFLINE/ON_OFFLINE)
 
 
-    @Builder
-    public Post(Long id, User user, String title, String postContent, PostStatus postStatus,
-                PostType postType, PostMeeting postMeeting) {
-        this.id = id;
-        this.user = user;
-        this.title = title;
-        this.postContent = postContent;
-        this.postStatus = postStatus;
-        this.postType = postType;
-        this.postMeeting = postMeeting;
-    }
+  public static Post createNewPost(User user, String title, String content, PostType postType,
+      PostMeeting postMeeting) {
+    return Post.builder()
+        .user(user)
+        .title(title)
+        .postContent(content)
+        .postType(postType)
+        .postMeeting(postMeeting)
+        .postStatus(PostStatus.CREATED)
+        .build();
+  }
+
+
+  @Builder
+  public Post(Long id, User user, String title, String postContent, PostStatus postStatus,
+      PostType postType, PostMeeting postMeeting) {
+    this.id = id;
+    this.user = user;
+    this.title = title;
+    this.postContent = postContent;
+    this.postStatus = postStatus;
+    this.postType = postType;
+    this.postMeeting = postMeeting;
+  }
 
 
 }
