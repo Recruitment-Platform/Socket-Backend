346a896155f4cc9fdcd983b10e84eec13996fc7c
hojoon <ujuj0202@gmail.com>
2023-11-03 16:31:35 +0900

    #43 [FEAT] Enum 타입 문서화를 위한 인터페이스 설정

--- src/main/java/com/project/socket/comment/model/CommentStatus.java
+++ src/main/java/com/project/socket/comment/model/CommentStatus.java
@@ -1,5 +1,22 @@
 package com.project.socket.comment.model;
 
-public enum CommentStatus {
-  CREATED, MODIFIED, DELETED
+import com.project.socket.common.model.EnumType;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public enum CommentStatus implements EnumType {
+  CREATED("생성"),
+  MODIFIED("수정"),
+  DELETED("삭제");
+
+  private final String description;
+  @Override
+  public String getName() {
+    return this.description;
+  }
+
+  @Override
+  public String getDescription() {
+    return this.name();
+  }
 }
---
+++ src/main/java/com/project/socket/common/model/EnumType.java
@@ -0,0 +1,6 @@
+package com.project.socket.common.model;
+
+public interface EnumType {
+  String getName();
+  String getDescription();
+}
