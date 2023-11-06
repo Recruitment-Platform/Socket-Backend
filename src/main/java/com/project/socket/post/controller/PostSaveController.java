c4e9dfefc9cd601ea70b661395b4e4c5688b520a
ShinMinOh <luciano97@naver.com>
2023-11-06 15:36:10 +0900

    [REFACTOR] 서비스 직접주입 받는 대신 인터페이스 주입으로 변경

--- src/main/java/com/project/socket/post/controller/PostSaveController.java
+++ src/main/java/com/project/socket/post/controller/PostSaveController.java
@@ -2,7 +2,7 @@ package com.project.socket.post.controller;
 
 import com.project.socket.post.controller.dto.request.PostSaveRequestDto;
 import com.project.socket.post.model.Post;
-import com.project.socket.post.service.PostSaveService;
+import com.project.socket.post.service.usecase.PostSaveUseCase;
 import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -19,7 +19,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 @Validated
 public class PostSaveController {
 
-  private final PostSaveService postSaveService;
+  private final PostSaveUseCase PostSaveUseCase;
 
   @PostMapping("/posts")
   public ResponseEntity<Object> postSave(
@@ -28,8 +28,8 @@ public class PostSaveController {
 
     long userId = Long.parseLong(userDetails.getUsername());
 
-    Post savedPost = postSaveService.createPost(
-        postSaveRequestDto.toSave(userId));
+    Post savedPost = PostSaveUseCase.createPost(
+        postSaveRequestDto.toCommand(userId));
 
     URI location = ServletUriComponentsBuilder.fromCurrentRequest()
         .path("/{id}")
