3cf2ee22fc3bb1059ce855d09d04309b0680db4b
ShinMinOh <luciano97@naver.com>
2023-11-06 16:01:13 +0900

    [FEAT] toEntity() 추가

--- src/main/java/com/project/socket/post/service/usecase/PostSaveCommand.java
+++ src/main/java/com/project/socket/post/service/usecase/PostSaveCommand.java
@@ -1,10 +1,12 @@
 package com.project.socket.post.service.usecase;
 
+import com.project.socket.post.model.Post;
 import com.project.socket.post.model.PostMeeting;
+import com.project.socket.post.model.PostStatus;
 import com.project.socket.post.model.PostType;
-import lombok.Builder;
+import com.project.socket.user.model.User;
 
-public record PostSaveInfo(
+public record PostSaveCommand(
     String title,
     String postContent,
     PostType postType,
@@ -13,8 +15,15 @@ public record PostSaveInfo(
 
 ) {
 
-  public PostSaveInfo toEntity() {
-      return PostSaveInfo.builder()
-          .
+  public Post toEntity(User user) {
+    return Post.builder()
+        .user(user)
+        .title(title)
+        .postContent(postContent)
+        .postType(postType)
+        .postMeeting(postMeeting)
+        .postStatus(PostStatus.CREATED)
+        .build();
+
   }
 }
