6112a3d8cd8b3876022421ee58550b1c063ca372
ShinMinOh <luciano97@naver.com>
2023-12-07 16:03:24 +0900

    [REFACTOR] @Transactional 어노테이션 삭제

--- src/main/java/com/project/socket/post/service/usecase/PostModifyUseCase.java
+++ src/main/java/com/project/socket/post/service/usecase/PostModifyUseCase.java
@@ -1,11 +1,10 @@
 package com.project.socket.post.service.usecase;
 
 import com.project.socket.post.model.Post;
-import jakarta.transaction.Transactional;
 
 public interface PostModifyUseCase {
 
-  @Transactional
+
   Post modifyPostNew(PostModifyCommand postModifyCommand);
 
 }
