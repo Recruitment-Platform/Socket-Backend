bd5a4e947380222cefb5a013eaa6031b7b0822cc
HoJoon Lim <ujuj0202@gmail.com>
2023-12-22 16:26:11 +0900

    [FEAT] REDIS 추가  (#63)
    * [FEAT] #60 stomp test를 위한 cors origin 추가
    
    * [FEAT] #60 redis 설정 추가
    
    * [TEST] #60 redis 컨테이너 연결 테스트
    
    * [REFACTOR] #60 제네릭 와일드카드 -> 명시적 타입으로 변경

---
+++ src/main/java/com/project/socket/config/RedisConfig.java
@@ -0,0 +1,30 @@
+package com.project.socket.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@RequiredArgsConstructor
+public class RedisConfig {
+
+  private final RedisProperties redisProperties;
+
+  @Bean
+  public RedisConnectionFactory redisConnectionFactory(){
+    return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
+  }
+
+  @Bean
+  public RedisTemplate<String, Object> redisTemplate(){
+    RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+    redisTemplate.setConnectionFactory(redisConnectionFactory());
+    redisTemplate.setDefaultSerializer(new StringRedisSerializer());
+    return redisTemplate;
+  }
+}
