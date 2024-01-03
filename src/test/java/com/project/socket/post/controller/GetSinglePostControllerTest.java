f47cc99f5bc6cf5bfc92f364575388896f7921c0
ShinMinOh <luciano97@naver.com>
2024-01-03 11:44:03 +0900

    [Test]테스트 코드 추가, 테스트 코드 리팩토링

--- src/test/java/com/project/socket/post/controller/GetSinglePostControllerTest.java
+++ src/test/java/com/project/socket/post/controller/GetSinglePostControllerTest.java
@@ -18,8 +18,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.socket.common.annotation.CustomWebMvcTestWithRestDocs;
 import com.project.socket.docs.DocumentLinkGenerator;
 import com.project.socket.docs.DocumentLinkGenerator.DocUrl;
+import com.project.socket.post.exception.PostDeletedException;
+import com.project.socket.post.exception.PostNotFoundException;
+import com.project.socket.post.model.PostMeeting;
 import com.project.socket.post.model.PostStatus;
-import com.project.socket.post.service.usecase.GetAllPostsUseCase;
+import com.project.socket.post.model.PostType;
+import com.project.socket.post.service.usecase.GetSinglePostUseCase;
 import com.project.socket.post.service.usecase.PostDto;
 import java.time.LocalDateTime;
 import org.junit.jupiter.api.Test;
@@ -28,8 +32,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
 
-@CustomWebMvcTestWithRestDocs(GetAllPostController.class)
-public class GetAllPostsControllerTest {
+@CustomWebMvcTestWithRestDocs(GetSinglePostController.class)
+public class GetSinglePostControllerTest {
 
   @Autowired
   MockMvc mockMvc;
@@ -38,14 +42,14 @@ public class GetAllPostsControllerTest {
   ObjectMapper objectMapper;
 
   @MockBean
-  GetAllPostsUseCase getAllPostsUseCase;
+  GetSinglePostUseCase getSinglePostUseCase;
 
   final Long POST_ID = 1L;
 
 
   @Test
   void 요청이_유효하면_포스트_아이디에_해당하는_게시물_조회와_200_응답을_한다() throws Exception {
-    when(getAllPostsUseCase.getSinglePost(anyLong())).thenReturn(createUseCaseResult());
+    when(getSinglePostUseCase.getPostDetail(anyLong())).thenReturn(createUseCaseResult());
 
     mockMvc.perform(get("/posts/{postId}", POST_ID))
         .andExpect(status().isOk())
@@ -60,11 +64,12 @@ public class GetAllPostsControllerTest {
                 fieldWithPath("postId").type(JsonFieldType.NUMBER).description("포스트 ID"),
                 fieldWithPath("title").type(JsonFieldType.STRING).description("포스트 제목"),
                 fieldWithPath("postContent").type(JsonFieldType.STRING).description("포스트 본문 내용"),
-                fieldWithPath("postType").type(JsonFieldType.STRING).description("PROJECT/STUDY"),
-                fieldWithPath("postMeeting").type(JsonFieldType.STRING)
-                    .description("ONLINE/OFFLINE/ON_OFFLINE"),
+                fieldWithPath("postType").description(
+                    DocumentLinkGenerator.generateLinkCode(DocUrl.POST_TYPE)),
+                fieldWithPath("postMeeting").description(
+                    DocumentLinkGenerator.generateLinkCode(DocUrl.POST_MEETING)),
                 fieldWithPath("postStatus").description(
-                    DocumentLinkGenerator.generateLinkCode(DocUrl.COMMENT_STATUS)),
+                    DocumentLinkGenerator.generateLinkCode(DocUrl.POST_STATUS)),
                 fieldWithPath("userId").description("게시물 작성자 ID"),
                 fieldWithPath("userNickname").description("게시물 작성자 닉네임"),
                 fieldWithPath("createdAt").type(JsonFieldType.STRING).description("게시물 생성일")
@@ -72,7 +77,7 @@ public class GetAllPostsControllerTest {
   }
 
   private PostDto createUseCaseResult() {
-    return new PostDto(1L, "title", "content", "PROJECT", "ONLINE",
+    return new PostDto(1L, "title", "content", PostType.STUDY, PostMeeting.ONLINE,
         PostStatus.CREATED, 1L, "nickname", LocalDateTime.now());
   }
 
@@ -80,6 +85,22 @@ public class GetAllPostsControllerTest {
   void postId_parameter가_1보다_작으면_400_응답을_한다() throws Exception {
     mockMvc.perform(get("/posts/{postId}", -1L))
         .andExpect(status().isBadRequest());
+  }
+
+  @Test
+  void 조회결과가_없어_PostNotFoundException_예외가_발생하면_404_응답을_한다() throws Exception {
+    when(getSinglePostUseCase.getPostDetail(anyLong())).thenThrow(new PostNotFoundException(1L));
+
+    mockMvc.perform(get("/posts/{postId}", 1L))
+        .andExpect(status().isNotFound());
+  }
+
+  @Test
+  void 조회결과_게시물이_삭제되어_PostDeletedException_예외가_발생하면_404_응답을_한다() throws Exception {
+    when(getSinglePostUseCase.getPostDetail(anyLong())).thenThrow(new PostDeletedException(1L));
+
+    mockMvc.perform(get("/posts/{postId}", 1L))
+        .andExpect(status().isNotFound());
   }
 
 }
--- src/test/java/com/project/socket/post/service/GetSinglePostDtoServiceTest.java
+++ src/test/java/com/project/socket/post/service/GetSinglePostDtoServiceTest.java
@@ -6,8 +6,11 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
+import com.project.socket.post.exception.PostDeletedException;
 import com.project.socket.post.exception.PostNotFoundException;
+import com.project.socket.post.model.PostMeeting;
 import com.project.socket.post.model.PostStatus;
+import com.project.socket.post.model.PostType;
 import com.project.socket.post.repository.PostJpaRepository;
 import com.project.socket.post.service.usecase.PostDto;
 import java.time.LocalDateTime;
@@ -23,31 +26,31 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayNameGeneration(ReplaceUnderscores.class)
-class GetAllPostDtoServiceTest {
+class GetSinglePostDtoServiceTest {
 
   final Long POST_ID = 1L;
 
   @InjectMocks
-  GetAllPostsService getAllPostsService;
+  GetSinglePostService getSinglePostService;
 
   @Mock
   PostJpaRepository postJpaRepository;
 
   @Test
-  void 조회결과가_없으면_IllegalArgumentException_예외가_발생한다() {
+  void 조회결과가_없으면_PostNotFoundException_예외가_발생한다() {
     when(postJpaRepository.findPostByPostId(anyLong())).thenReturn(Optional.empty());
 
-    assertThatThrownBy(() -> getAllPostsService.getSinglePost(POST_ID))
-        .isInstanceOf(IllegalArgumentException.class);
+    assertThatThrownBy(() -> getSinglePostService.getPostDetail(POST_ID))
+        .isInstanceOf(PostNotFoundException.class);
   }
 
   @Test
-  void 조회결과_postStatus가_DELETED일_경우_PostNotFoundException_예외가_발생한다() {
+  void 조회결과_postStatus가_DELETED일_경우_PostDeletedException_예외가_발생한다() {
     when(postJpaRepository.findPostByPostId(anyLong())).
         thenReturn(Optional.of(sampleDto().get(0)));
 
-    assertThatThrownBy(() -> getAllPostsService.getSinglePost(POST_ID))
-        .isInstanceOf(PostNotFoundException.class);
+    assertThatThrownBy(() -> getSinglePostService.getPostDetail(POST_ID))
+        .isInstanceOf(PostDeletedException.class);
   }
 
   @Test
@@ -55,22 +58,23 @@ class GetAllPostDtoServiceTest {
     when(postJpaRepository.findPostByPostId(anyLong())).
         thenReturn(Optional.of(sampleDto().get(1)));
 
-    PostDto searchPost = getAllPostsService.getSinglePost(POST_ID);
+    PostDto searchPost = getSinglePostService.getPostDetail(POST_ID);
 
     assertAll(
         () -> assertThat(searchPost.getPostId()).isEqualTo(2L),
         () -> assertThat(searchPost.getTitle()).isEqualTo("title"),
         () -> assertThat(searchPost.getPostContent()).isEqualTo("content"),
-        () -> assertThat(searchPost.getPostType()).isEqualTo("STUDY"),
-        () -> assertThat(searchPost.getPostMeeting()).isEqualTo("OFFLINE")
+        () -> assertThat(searchPost.getPostType()).isEqualTo(PostType.STUDY),
+        () -> assertThat(searchPost.getPostMeeting()).isEqualTo(PostMeeting.OFFLINE)
     );
   }
 
   List<PostDto> sampleDto() {
     return List.of(
-        new PostDto(1L, "title", "content", "PROJECT", "ONLINE", PostStatus.DELETED,
+        new PostDto(1L, "title", "content", PostType.PROJECT, PostMeeting.ONLINE,
+            PostStatus.DELETED,
             1L, "nickname", LocalDateTime.now()),
-        new PostDto(2L, "title", "content", "STUDY", "OFFLINE", PostStatus.CREATED,
+        new PostDto(2L, "title", "content", PostType.STUDY, PostMeeting.OFFLINE, PostStatus.CREATED,
             1L, "nickname", LocalDateTime.now())
     );
   }
