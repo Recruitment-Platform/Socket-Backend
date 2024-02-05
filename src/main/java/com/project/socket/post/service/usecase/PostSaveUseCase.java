a6d45fe1c3f4fa0331110f750bd96ac7ba1b2504
ShinMinOh <luciano97@naver.com>
2024-02-06 03:23:12 +0900

    [Refactor] 인터페이스 @Transactional 어노테이션 제거

--- src/main/java/com/project/socket/post/service/usecase/PostSaveUseCase.java
+++ src/main/java/com/project/socket/post/service/usecase/PostSaveUseCase.java
@@ -2,13 +2,10 @@ package com.project.socket.post.service.usecase;
 
 import com.project.socket.post.model.Post;
 import java.util.List;
-import org.springframework.transaction.annotation.Transactional;
 
 public interface PostSaveUseCase {
 
-  @Transactional
   Post createPost(PostSaveCommand postSaveInfo);
 
-  @Transactional
   void mapPostWithSkill(Post post, List<String> skillNames);
 }
