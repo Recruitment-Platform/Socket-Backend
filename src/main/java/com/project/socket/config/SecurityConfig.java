970acdcbb2107eb569530f8aaa0ded1e4c75e1c1
hojoon <ujuj0202@gmail.com>
2024-03-12 00:42:28 +0900

    #75 [FEAT] static resource ignore 추가

--- src/main/java/com/project/socket/config/SecurityConfig.java
+++ src/main/java/com/project/socket/config/SecurityConfig.java
@@ -13,11 +13,13 @@ import java.util.Arrays;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -70,6 +72,12 @@ public class SecurityConfig {
 
   @Value("${cors.allow-origins}")
   private List<String> corsOrigins;
+
+  @Bean
+  public WebSecurityCustomizer webSecurityCustomizer(){
+    return web -> web.ignoring()
+                     .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+  }
 
   @Bean
   CorsConfigurationSource corsConfigurationSource() {
