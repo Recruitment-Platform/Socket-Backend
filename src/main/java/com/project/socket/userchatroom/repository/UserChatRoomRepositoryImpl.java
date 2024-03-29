177b72e89508dcfc77a7f916b75c6fbfd497cdb5
hojoon <ujuj0202@gmail.com>
2024-03-29 19:46:42 +0900

    #82 [FEAT] 안읽음 카운트 증가 로직추가

--- src/main/java/com/project/socket/userchatroom/repository/UserChatRoomRepositoryImpl.java
+++ src/main/java/com/project/socket/userchatroom/repository/UserChatRoomRepositoryImpl.java
@@ -4,14 +4,17 @@ import static com.project.socket.chatroom.model.QChatRoom.chatRoom;
 import static com.project.socket.userchatroom.model.QUserChatRoom.userChatRoom;
 
 import com.project.socket.userchatroom.model.UserChatRoom;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 public class UserChatRoomRepositoryImpl implements UserChatRoomRepositoryCustom {
 
   private final JPAQueryFactory jpaQueryFactory;
 
+  @Transactional
   @Override
   public UserChatRoom findByUserIdAndPostId(Long userId, Long postId) {
     return jpaQueryFactory.selectFrom(userChatRoom)
@@ -19,5 +22,22 @@ public class UserChatRoomRepositoryImpl implements UserChatRoomRepositoryCustom 
                           .where(userChatRoom.user.userId.eq(userId).and(
                               chatRoom.post.id.eq(postId)))
                           .fetchOne();
+  }
+
+  @Transactional
+  @Override
+  public void updateUnreadCount(Long userId, Long chatRoomId) {
+    jpaQueryFactory.update(userChatRoom)
+        .set(userChatRoom.unreadCount, userChatRoom.unreadCount.add(1))
+        .where(eqUserId(userId).and(eqChatRoomId(chatRoomId)))
+        .execute();
+  }
+
+  private BooleanExpression eqChatRoomId(Long chatRoomId) {
+    return chatRoomId != null ? userChatRoom.chatRoom.chatRoomId.eq(chatRoomId) : null;
+  }
+
+  private BooleanExpression eqUserId(Long userId) {
+    return userId != null ? userChatRoom.user.userId.eq(userId) : null;
   }
 }
